a8961342c931e8104133c0246ec7ab20d400988e
snusnu <gamsnjaga@gmail.com>
2010-08-20 18:03:18 +0200

    Added specs for DataObjects::Schema::Tables#each

---
+++ spec/unit/data_objects/schema/tables/each_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'do-schema/table'
+
+describe 'DataObjects::Schema::Tables' do
+
+  subject { DataObjects::Schema::Tables.new }
+
+  it { should be_kind_of(Enumerable) }
+
+  it 'case matches Enumerable' do
+    (Enumerable === subject).should be(true)
+  end
+end
+
+describe 'DataObjects::Schema::Tables#each' do
+
+  subject { tables.each { |c| yields << c } }
+
+  let(:tables) { DataObjects::Schema::Tables.new([table]) }
+  let(:table)  { DataObjects::Schema::Table.new('name')      }
+  let(:yields) { []                                               }
+
+  it { should equal(tables) }
+
+  it 'yields each table' do
+    expect { subject }.to change { yields.dup }.from([]).to([table])
+  end
+end
+
