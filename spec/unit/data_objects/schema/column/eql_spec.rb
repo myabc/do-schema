0db9f892f1ff55294507f287d8146f85d3c3f073
snusnu <gamsnjaga@gmail.com>
2010-08-20 16:46:08 +0200

    More Column#== and Column#eql? specs - 3 mutations left

--- spec/unit/data_objects/schema/column/eql_spec.rb
+++ spec/unit/data_objects/schema/column/eql_spec.rb
@@ -3,11 +3,11 @@ require 'do-schema/column'
 
 describe 'DataObjects::Schema::Column#eql?' do
 
-  subject { column == other }
+  subject { column.eql?(other) }
 
-  let(:name)    { 'foo'                                          }
-  let(:options) { {}                                             }
-  let(:column)  { DataObjects::Schema::Column.new(name, options) }
+  let(:name)             { 'foo'                                                   }
+  let(:original_options) { { :default => 'foo', :required => true }                }
+  let(:column)           { DataObjects::Schema::Column.new(name, original_options) }
 
   context 'with the same column' do
 
@@ -31,15 +31,88 @@ describe 'DataObjects::Schema::Column#eql?' do
     end
   end
 
-  context 'with a different column name' do
+  context 'with a column subclass that is otherwise equal' do
 
-    let(:other) { DataObjects::Schema::Column.new('different name', options) }
+    let(:other) { Class.new(DataObjects::Schema::Column).new(name, original_options) }
 
     it { should be(false) }
 
     it 'is symmetric' do
       should == other.eql?(column)
     end
+  end
+
+  context 'with aa column subclass that is otherwise equal' do
+
+    let(:other) { Class.new(DataObjects::Schema::Column).new(name, options) }
+    let(:options) { { :default => 'foo', :required => true } }
+
+    it { should be(false) }
+
+    it 'is symmetric' do
+      should == other.eql?(column)
+    end
+  end
+
+  context 'with a different name and the same options' do
+
+    let(:other) { DataObjects::Schema::Column.new('different name', original_options) }
+
+    it { should be(false) }
+
+    it 'is symmetric' do
+      should == other.eql?(column)
+    end
+  end
+
+  context 'with a different name, a different default value and the same required status' do
+
+    let(:other)   { DataObjects::Schema::Column.new('different name', options) }
+    let(:options) { { :default => 'bar', :required => true } }
+
+    it { should be(false) }
+
+    it 'is symmetric' do
+      should == other.eql?(column)
+    end
+  end
+
+  context 'with a different name, a different default value and a different required status' do
+
+    let(:other)   { DataObjects::Schema::Column.new('different name', options) }
+    let(:options) { { :default => 'bar', :required => false } }
+
+    it { should be(false) }
+
+    it 'is symmetric' do
+      should == other.eql?(column)
+    end
+  end
+
+  context 'with the same name, a different default value and the same require status' do
+
+    let(:other)   { DataObjects::Schema::Column.new(name, options) }
+    let(:options) { { :default => 'bar', :required => true } }
+
+    it { should be(false) }
+
+    it 'is symmetric' do
+      should == other.eql?(column)
+    end
+
+  end
+
+  context 'with the same name, the same default value and a different required status' do
+
+    let(:other)   { DataObjects::Schema::Column.new(name,options) }
+    let(:options) { { :default => 'foo', :required => false } }
+
+    it { should be(false) }
+
+    it 'is symmetric' do
+      should == other.eql?(column)
+    end
+
   end
 
 end
--- spec/unit/data_objects/schema/column/equal_value_spec.rb
+++ spec/unit/data_objects/schema/column/equal_value_spec.rb
@@ -5,9 +5,9 @@ describe 'DataObjects::Schema::Column#==' do
 
   subject { column == other }
 
-  let(:name)    { 'foo'                                          }
-  let(:options) { {}                                             }
-  let(:column)  { DataObjects::Schema::Column.new(name, options) }
+  let(:name)             { 'foo'                                                   }
+  let(:original_options) { { :default => 'foo', :required => true }                }
+  let(:column)           { DataObjects::Schema::Column.new(name, original_options) }
 
   context 'with the same column' do
 
@@ -31,15 +31,76 @@ describe 'DataObjects::Schema::Column#==' do
     end
   end
 
-  context 'with a different column name' do
+  context 'with a column subclass that is otherwise equal' do
 
-    let(:other) { DataObjects::Schema::Column.new('different name', options) }
+    let(:other) { Class.new(DataObjects::Schema::Column).new(name, original_options) }
+
+    it { should be(true) }
+
+    it 'is symmetric' do
+      should == (other == column)
+    end
+  end
+
+  context 'with a different name and the same options' do
+
+    let(:other) { DataObjects::Schema::Column.new('different name', original_options) }
 
     it { should be(false) }
 
     it 'is symmetric' do
       should == (other == column)
     end
+  end
+
+  context 'with a different name, a different default value and the same required status' do
+
+    let(:other)   { DataObjects::Schema::Column.new('different name', options) }
+    let(:options) { { :default => 'bar', :required => true } }
+
+    it { should be(false) }
+
+    it 'is symmetric' do
+      should == (other == column)
+    end
+  end
+
+  context 'with a different name, a different default value and a different required status' do
+
+    let(:other)   { DataObjects::Schema::Column.new('different name', options) }
+    let(:options) { { :default => 'bar', :required => false } }
+
+    it { should be(false) }
+
+    it 'is symmetric' do
+      should == (other == column)
+    end
+  end
+
+  context 'with the same name, a different default value and the same require status' do
+
+    let(:other)   { DataObjects::Schema::Column.new(name, options) }
+    let(:options) { { :default => 'bar', :required => true } }
+
+    it { should be(false) }
+
+    it 'is symmetric' do
+      should == (other == column)
+    end
+
+  end
+
+  context 'with the same name, the same default value and a different required status' do
+
+    let(:other)   { DataObjects::Schema::Column.new(name,options) }
+    let(:options) { { :default => 'foo', :required => false } }
+
+    it { should be(false) }
+
+    it 'is symmetric' do
+      should == (other == column)
+    end
+
   end
 
 end
