8d9ddafd027a95be6528ae3d4426255e68ca6d92
snusnu <gamsnjaga@gmail.com>
2010-08-21 01:19:46 +0200

    Added specs for DataObjects::Schema::Table#initialize

---
+++ spec/unit/data_objects/schema/table/initialize_spec.rb
@@ -0,0 +1,32 @@
+ require 'spec_helper'
+ require 'do-schema/table'
+
+ describe 'DataObjects::Schema::Table#initialize' do
+
+   let(:name) { 'name' }
+
+   shared_examples_for 'calling DataObjects::Schema::Table#initialize' do
+
+     its(:name)    { should == name }
+     its(:columns) { should == DataObjects::Schema::Columns.new([]) }
+
+   end
+
+   context 'when no columns are passed at construction time' do
+
+     subject { DataObjects::Schema::Table.new(name) }
+
+     it_should_behave_like 'calling DataObjects::Schema::Table#initialize'
+
+   end
+
+   context 'when an array is passed at construction time' do
+
+     subject { DataObjects::Schema::Table.new(name, []) }
+
+     it_should_behave_like 'calling DataObjects::Schema::Table#initialize'
+
+   end
+
+ end
+
