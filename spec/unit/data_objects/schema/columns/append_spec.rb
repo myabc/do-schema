53b74fe6843fb3a2343bdb8351872638f1e8275f
snusnu <gamsnjaga@gmail.com>
2010-08-21 20:07:28 +0200

    Fixed broken specs for append semantics
    * Really spec append semantics for Columns#<<
    * Really spec append semantics for Tables#<<

--- spec/unit/data_objects/schema/columns/append_spec.rb
+++ spec/unit/data_objects/schema/columns/append_spec.rb
@@ -5,17 +5,46 @@ describe 'DataObjects::Schema::Columns#<<' do
 
   subject { columns << column2 }
 
-  let(:column1)  { DataObjects::Schema::Column.new('name', {}) }
-  let(:column2)  { DataObjects::Schema::Column.new('name', {}) }
-
+  let(:column1)  { DataObjects::Schema::Column.new('column1', {}) }
   let(:columns)  { DataObjects::Schema::Columns.new([column1]) }
 
-  its(:entries) { should include(column1) }
-  its(:entries) { should include(column2) }
+  context 'when appending a not yet present column' do
 
-  it 'should append columns at the end of the collection' do
-    columns.entries.index(column1).should == columns.entries.length - 2
-    columns.entries.index(column2).should == columns.entries.length - 1
+    let(:column2)  { DataObjects::Schema::Column.new('column2', {}) }
+
+    its(:entries) { should include(column1) }
+    its(:entries) { should include(column2) }
+
+    it 'should not alter the position of the already existing column' do
+      subject.entries.index(column1).should == columns.entries.length - 2
+    end
+
+    it 'should append columns at the end of the collection' do
+      subject.entries.index(column2).should == columns.entries.length - 1
+    end
+
+    it 'adds one table to the collection' do
+      expect { subject }.to change { columns.entries.count }.from(1).to(2)
+    end
+
+
+  end
+
+  context 'when appending an already present column' do
+
+    let(:column2)  { column1.dup }
+
+    its(:entries) { should include(column1) }
+    its(:entries) { should include(column2) }
+
+    it 'should not alter the position of the already existing column' do
+      subject.entries.index(column1).should == columns.entries.length - 1
+    end
+
+    it 'does not add a table to the collection' do
+      expect { subject }.to_not change { columns.entries.count }
+    end
+
   end
 
 end
--- spec/unit/data_objects/schema/tables/append_spec.rb
+++ spec/unit/data_objects/schema/tables/append_spec.rb
@@ -5,17 +5,33 @@ describe 'DataObjects::Schema::Tables#<<' do
 
   subject { tables << table2 }
 
-  let(:table1)  { DataObjects::Schema::Column.new('column1', {}) }
-  let(:table2)  { DataObjects::Schema::Column.new('column2', {}) }
+  let(:table1)  { DataObjects::Schema::Table.new('table1')  }
+  let(:tables)  { DataObjects::Schema::Tables.new([table1]) }
 
-  let(:tables)  { DataObjects::Schema::Columns.new([table1]) }
+  context 'when appending a not yet present table' do
 
-  its(:entries) { should include(table1) }
-  its(:entries) { should include(table2) }
+    let(:table2)  { DataObjects::Schema::Table.new('table2') }
 
-  it 'should append columns at the end of the collection' do
-    tables.entries.index(table1).should == tables.entries.length - 2
-    tables.entries.index(table2).should == tables.entries.length - 1
+    its(:entries) { should include(table1) }
+    its(:entries) { should include(table2) }
+
+    it 'adds one table to the collection' do
+      expect { subject }.to change { tables.entries.count }.from(1).to(2)
+    end
+
+  end
+
+  context 'when appending an already present table' do
+
+    let(:table2)  { table1.dup }
+
+    its(:entries) { should include(table1) }
+    its(:entries) { should include(table2) }
+
+    it 'does not add a table to the collection' do
+      expect { subject }.to_not change { tables.entries.count }
+    end
+
   end
 
 end
